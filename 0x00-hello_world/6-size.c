2fb5f9a00a7c215852d4af9fb5b7a41914341ce6
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-08 16:54:33 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  This should check size
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	printf('Size of an int: %i byte(s)\n', sizeof(int));
+	return (0);
