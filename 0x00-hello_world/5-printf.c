498f37b41546aa77dba74424a960806941146064
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-08 16:34:16 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This should use the printf function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
