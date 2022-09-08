fc03f06b76f3a8dec99093111ed6fe56ddd608a2
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-08 12:56:34 -0700

    puts on screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#include <stdio.h>
+/*
+ *
+ * print
+ *
+ * return: zero
+ *
+ */
+int main(void)
+{ 
+	char mainStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(mainStr);
+	return(0);
+}
