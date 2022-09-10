903de90dc3784173d854d83191055781eefc7a70
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-09 22:14:19 -0700

    done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,19 +20,17 @@ int main(void)
 			for (right = center + 1 ; right <= 57; right++)
 			{
 				putchar(left);
-
 				putchar (center);
-
 				putchar (right);
 				if  ((left == 55) && (center == left + 1) && (right == center + 1))
-	{
+				{
 					break;
 				}
 				putchar(',');
 				putchar (' ');
 			}
 		}
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
