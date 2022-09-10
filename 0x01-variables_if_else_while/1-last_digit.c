58f55f1beb6a9e49f04dfe2d2a616062a4de0edf
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-09 17:50:08 -0700

    done

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
