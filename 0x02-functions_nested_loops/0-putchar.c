a721558bce6fe5d1a784b722f24ba06113db2f83
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-13 07:20:37 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return:return 0
+ *
+ */
+
+int main(void)
+
+{
+	char alphabet[8] = "_putchar";
+	int b;
+
+	for (b = 0; b < 8; b++)
+	{
+		putchar(alphabet[b]);
+	}
+	putchar(8);
+	return (0);
+
+}
