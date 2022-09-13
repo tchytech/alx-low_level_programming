b0332898e48583c46633483345da8815ab7d718e
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-13 08:18:36 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int r;
+
+	for (r = 97; r <= 122; r++)
+	
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
