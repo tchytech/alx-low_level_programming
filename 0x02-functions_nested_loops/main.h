098e237a052c885b088807138b1d5e24b5bf6452
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-14 07:00:56 -0700

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-int _putchar(char c);
+#ifndef _main_h_
+
+#define _main_h_
+
+
+
+int _putchar(char);
 
 void print_alphabet(void);
 
@@ -10,14 +16,20 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
-int _abs(int r);
+int _abs(int);
 
-int print_last_digit(int j);
+int print_last_digit(int);
 
 void jack_bauer(void);
 
-void print_times_table(void);
+void times_table(void);
 
-int add(int i, int l);
+int add(int, int);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
