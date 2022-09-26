7f6e85306fbda1e464f33941c25db75193069bbf
Tochukwu Onah <onahpatience1610@gmail.com>
2022-09-26 07:27:19 -0700

    done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,25 +8,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int c = 0;
-	char *t = accept;
+	unsigned int i, j, bool;
 
-	while (*s++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		while (*accept++)
-		{
-			if (*(s - 1) == *(accept - 1))
-			{
-				c++;
-				break;
-			}
-			if (!(*--accept))
-			{
-				break;
-			}
-			accept = t;
-		}
+		bool = 1;
 
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+		{
+			break;
+		}
 	}
-	return (c);
+	return (i);
 }
