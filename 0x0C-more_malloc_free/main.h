33982a897a0451375a027f4e0ca83eff80060529
Tochukwu Onah <onahpatience1610@gmail.com>
2022-10-06 13:52:18 -0700

    done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,8 @@
-#ifndef MAIN_H
+#ifndef _MAIN_H_
 
-#define MAIN_H
+#define _MAIN_H_
 
 
-
-void _puts(char *str);
 
 void *malloc_checked(unsigned int b);
 
@@ -15,14 +13,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-int _putchar(char c);
-
-int _atoi(const char *s);
-
-void print_int(unsigned long int n);
-
-
 
 
 
