76ff63006a19f97847875067d8eb566c016a0469
Tochukwu Onah <onahpatience1610@gmail.com>
2022-10-06 08:59:16 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define ERR_MSG "Error"
-
-int _isdigit(char *s);
 
 /**
  * main - takes two numbers as arguments and outputs the product.
@@ -15,6 +14,7 @@ int _isdigit(char *s);
 int main(int argc, char *argv[])
 {
 	int i;
+
 	if (argc != 3)
 	{
 		for (i = 0; ERR_MSG[i] != '\0'; i++)
@@ -22,14 +22,14 @@ int main(int argc, char *argv[])
 		_putchar(10);
 		exit(98);
 	}
-	if (_isdigit(argv[1]) == 0)
+	if (isdigit(argv[1]) == 0)
 		printf("%s\n", argv[1]);
-		
+
 	return (0);
 }
 
 /**
- * isdigit - checks if string is a number or not
+ * _isdigit - checks if string is a number or not
  * @s: the string to be checked
  *
  * Return: 1 if true, else 0.
