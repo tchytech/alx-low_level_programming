783cf3e0e58c49e876a724c9b86175ada463e49c
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2022-10-13 08:28:49 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,21 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-
-#define FUNCTION_POINTERS_H
-
-
-
 #include <stdio.h>
 
-#include <stdlib.h>
-
-
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
-
-
-#endif /*MAIN_H*/
+#endif /* #ifndef FUNCTION_POINTERS_ */
